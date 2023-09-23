c39038151529a59ed0862ebe2a4503e60a0a04e7
dkp-consult <contact@dkp-consult.be>
2023-09-23 17:33:11 +0200

    dev : pagination translation

--- components/Pagination.js
+++ components/Pagination.js
@@ -9,25 +9,25 @@ export default function Pagination({ totalPages, currentPage }) {
       <nav className="flex justify-between">
         {!prevPage && (
           <button rel="previous" className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
-            Previous
+            Précédents
           </button>
         )}
         {prevPage && (
           <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}>
-            <button rel="previous">Previous</button>
+            <button rel="previous">Précédents</button>
           </Link>
         )}
         <span>
-          {currentPage} of {totalPages}
+          Page {currentPage} sur {totalPages}
         </span>
         {!nextPage && (
           <button rel="next" className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
-            Next
+            Suivants
           </button>
         )}
         {nextPage && (
           <Link href={`/blog/page/${currentPage + 1}`}>
-            <button rel="next">Next</button>
+            <button rel="next">Suivants</button>
           </Link>
         )}
       </nav>
--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -91,7 +91,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
-              <div className="space-x-3 pt-6 pb-6 text-center text-sm text-gray-700 dark:text-gray-300 flex-column">
+              <div className="flex-column space-x-3 pt-6 pb-6 text-center text-sm text-gray-700 dark:text-gray-300">
                 <p className="pb-6">{"Partagez l'article :"}</p>{' '}
                 {/*                 <Link href={discussUrl(slug)} rel="nofollow">
                   {'Discuss on Twitter'}
