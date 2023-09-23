bd629726f657cf42aa91ac50483ed6085e2a8ccd
dkp-consult <contact@dkp-consult.be>
2023-09-23 17:04:31 +0200

    dev : edit metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,8 @@ const siteMetadata = {
   title: 'dkp-consult - le blog',
   author: 'Pierre Debski',
   headerTitle: '',
-  description: 'Le blog d\'un développeur en devenir',
+  // eslint-disable-next-line prettier/prettier
+  description: 'Le blog d\'un développeur',
   language: 'fr-be',
   theme: 'system', // system, dark or light
   siteUrl: 'https://blog.dkp-consult.be',
@@ -13,9 +14,9 @@ const siteMetadata = {
   email: 'contact@dkp-consult.be',
   github: 'https://github.com/dkp-consult',
   twitter: 'https://twitter.com/dkp_consult',
-  //  facebook: 'https://facebook.com',
+  facebook: 'https://www.facebook.com/dkpconsult',
   //  youtube: 'https://youtube.com',
-  //  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/pierre-debski/',
   locale: 'fr-BE',
   analytics: {},
   newsletter: {},
